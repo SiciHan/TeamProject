b2744c8893ec1b535dcfccf5dfe6a7976898fd69
James Cheang <cheang.james@gmail.com>
2019-12-05 22:42:16 +0800

    merging student service, implementation and commenting out classes

--- Team8SMS/src/main/java/sg/nus/iss/team8/demo/services/StudentService.java
+++ Team8SMS/src/main/java/sg/nus/iss/team8/demo/services/StudentService.java
@@ -2,10 +2,18 @@ package sg.nus.iss.team8.demo.services;
 
 import java.util.ArrayList;
 
+import org.springframework.stereotype.Service;
+
+import sg.nus.iss.team8.demo.models.CourserunStudent;
 import sg.nus.iss.team8.demo.models.Student;
 
+@Service
 public interface StudentService {
-	
+	public ArrayList<CourserunStudent> findAvailableCourserunStudents(int studentid);
+
+	public ArrayList<CourserunStudent> findPendingCourserunStudents(int studentid);
+
+	public ArrayList<CourserunStudent> findRejectedAndApprovedCourserunStudents(int studentid);
 	ArrayList<Student> findAllStudents();
 	
 	Student findStudent(int id);
--- Team8SMS/src/main/java/sg/nus/iss/team8/demo/services/StudentServiceImplementation.java
+++ Team8SMS/src/main/java/sg/nus/iss/team8/demo/services/StudentServiceImplementation.java
@@ -1,28 +1,43 @@
 package sg.nus.iss.team8.demo.services;
+import java.util.ArrayList;
+import java.util.logging.Filter;
 
 import java.util.ArrayList;
 
 import javax.annotation.Resource;
 import javax.transaction.Transactional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.stereotype.Service;
 
 import sg.nus.iss.team8.demo.models.Student;
+import sg.nus.iss.team8.demo.models.CourserunStudent;
+import sg.nus.iss.team8.demo.repositories.CourserunStudentRepository;
 import sg.nus.iss.team8.demo.repositories.StudentRepository;
-
-@Service
-public class StudentServiceImplementation implements StudentService {
+ 
+@Service 
+public class StudentServiceImplementation implements StudentService{
 	@Resource
 	private StudentRepository studentRepository;
-
-	public StudentRepository getSr() {
-		return studentRepository;
-	}
+	private CourserunStudentRepository courserunStudentRepository;
+	
 	@Autowired
-
-	public void setStudentRepository(StudentRepository sr) {
-		this.studentRepository = sr;
+	public void setStudentRepository(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository; 
+	}
+	
+	@Autowired
+	public void setCourserunStudentRepository(CourserunStudentRepository courserunStudentRepository) {
+		this.courserunStudentRepository=courserunStudentRepository;
+		
+	}
+	
+	@Override
+	public ArrayList<CourserunStudent> findAvailableCourserunStudents(int studentid) {
+		// TODO Auto-generated method stub
+		//Available =Cancelled; Status =5
+		ArrayList<CourserunStudent> courses = (ArrayList<CourserunStudent>) courserunStudentRepository.findCourseByIdAndStatus(studentid, 5);
+		return courses;
 	}
 	
 	@Override
@@ -49,11 +64,25 @@ public class StudentServiceImplementation implements StudentService {
 	public Student updateStudent(Student student) {
 		return studentRepository.saveAndFlush(student);
 	}
+	@Override
+	public ArrayList<CourserunStudent> findPendingCourserunStudents(int studentid) {
+		// TODO Auto-generated method stub
+		//statusid 4 = Pending
+		ArrayList<CourserunStudent> courses = (ArrayList<CourserunStudent>) courserunStudentRepository.findCourseByIdAndStatus(studentid,4);
+		return courses;
+	}
 	
 	@Override
 	@Transactional
 	public void removeStudent(Student student) {
 		studentRepository.delete(student);
 	}
-
+	@Override
+	public ArrayList<CourserunStudent> findRejectedAndApprovedCourserunStudents(int studentid) {
+		// TODO Auto-generated method stub
+		//status id 7 =rejected, statusid 6 =Approved
+		ArrayList<CourserunStudent> courses = (ArrayList<CourserunStudent>) courserunStudentRepository.findCourseByIdAndStatus(studentid, 6);
+		courses.addAll(courserunStudentRepository.findCourseByIdAndStatus(studentid,7));
+		return courses;
+	}	
 }
