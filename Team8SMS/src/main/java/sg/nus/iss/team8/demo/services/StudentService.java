ca6c2de41a10249c0549aa80bc020fcef08233b1
SiciHan <32950564+SiciHan@users.noreply.github.com>
2019-12-05 15:32:09 +0800

    Student Repository and Service Layer

--- Team8SMS/src/main/java/sg/nus/iss/team8/demo/services/StudentService.java
+++ Team8SMS/src/main/java/sg/nus/iss/team8/demo/services/StudentService.java
@@ -1,5 +1,13 @@
 package sg.nus.iss.team8.demo.services;
 
+import java.util.ArrayList;
+
+import sg.nus.iss.team8.demo.models.CourserunStudent;
+
 public interface StudentService {
-	
+	public ArrayList<CourserunStudent> findAvailableCourserunStudents(int studentid);
+
+	public ArrayList<CourserunStudent> findPendingCourserunStudents(int studentid);
+
+	public ArrayList<CourserunStudent> findRejectedAndApprovedCourserunStudents(int studentid);
 }
--- Team8SMS/src/main/java/sg/nus/iss/team8/demo/services/StudentServiceImplementation.java
+++ Team8SMS/src/main/java/sg/nus/iss/team8/demo/services/StudentServiceImplementation.java
@@ -1,23 +1,43 @@
 package sg.nus.iss.team8.demo.services;
+import java.util.ArrayList;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.stereotype.Service;
 
+import sg.nus.iss.team8.demo.models.CourserunStudent;
+import sg.nus.iss.team8.demo.repositories.CourserunStudentRepository;
 import sg.nus.iss.team8.demo.repositories.StudentRepository;
-
-@Service
-public class StudentServiceImplementation implements StudentService {
+ 
+@Service 
+public class StudentServiceImplementation implements StudentService{
 	private StudentRepository studentRepository;
-
-	public StudentRepository getSr() {
-		return studentRepository;
-	}
+	private CourserunStudentRepository courserunStudentRepository;
+	
 	@Autowired
-
-	public void setStudentRepository(StudentRepository sr) {
-		this.studentRepository = sr;
+	public void setStudentRepository(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository; 
 	}
 	
+	@Autowired
+	public void setCourserunStudentRepository(CourserunStudentRepository courserunStudentRepository) {
+		this.courserunStudentRepository=courserunStudentRepository;
+	}
 	
-
+	@Override
+	public ArrayList<CourserunStudent> findAvailableCourserunStudents(int studentid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public ArrayList<CourserunStudent> findPendingCourserunStudents(int studentid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public ArrayList<CourserunStudent> findRejectedAndApprovedCourserunStudents(int studentid) {
+		// TODO Auto-generated method stub
+		return null;
+	}	
 }
