20973c81fbb15a6b4ff2a0f4b5cbd74d231b1686
phyu007 <sow17809@gmail.com>
2019-12-06 22:22:10 +0800

    Updated HomeController and Usersession

--- Team8SMS/src/main/java/sg/nus/iss/team8/demo/repositories/UserRepository.java
+++ Team8SMS/src/main/java/sg/nus/iss/team8/demo/repositories/UserRepository.java
@@ -1,9 +1,23 @@
-/*
- * package sg.nus.iss.team8.demo.repositories;
- * 
- * import org.springframework.data.jpa.repository.JpaRepository;
- * 
- * public interface UserRepository<User> extends JpaRepository<User,String> {
- * 
- * }
- */
+
+package sg.nus.iss.team8.demo.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import sg.nus.iss.team8.demo.models.CourserunStudent;
+import sg.nus.iss.team8.demo.models.User;
+import sg.nus.iss.team8.demo.models.UserSession;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer>{
+ 
+	
+	 @Query("select u from User u where u.username= :name")
+	 User findByUsername(@Param("name")String name);
+	
+}
