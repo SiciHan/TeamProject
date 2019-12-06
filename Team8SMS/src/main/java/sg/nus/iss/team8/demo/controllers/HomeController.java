91b0c5c0ec9a55109511281217bbdf11483954fb
phyu007 <sow17809@gmail.com>
2019-12-07 00:24:17 +0800

    Password Hash Updated.

--- Team8SMS/src/main/java/sg/nus/iss/team8/demo/controllers/HomeController.java
+++ Team8SMS/src/main/java/sg/nus/iss/team8/demo/controllers/HomeController.java
@@ -1,8 +1,7 @@
 package sg.nus.iss.team8.demo.controllers;
 
-import org.apache.commons.logging.Log;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,14 +15,17 @@ import org.springframework.web.bind.support.SessionStatus;
 import sg.nus.iss.team8.demo.models.*;
 import sg.nus.iss.team8.demo.services.UserService;
 import sg.nus.iss.team8.demo.services.UserServiceImplementation;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 
 
 @Controller
 public class HomeController {
 	
-
-
+   
+	 
+	 
 	  private UserService us;
 	  
 	  @Autowired 
@@ -55,6 +57,9 @@ public class HomeController {
 	@PostMapping("/authenticate")
 	public String getAuthentication(@ModelAttribute("user") UserSession user, BindingResult bindingResult) {
 		String view =""; 
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		
+	
 		if ( bindingResult.hasErrors())
 		{
 			view = "login";
@@ -64,9 +69,24 @@ public class HomeController {
 			String input = user.getName();
 			String password = user.getPassword();
 			sg.nus.iss.team8.demo.models.User usr = us.findUser(user.getName());
+			System.out.println(password);
 			
-			if ( input  == usr.getUsername() && password == usr.getPasswordHash())
-			{	view= "redirect:/student/applycourse"; }
+			if (input.equals(usr.getUsername()) )
+			{	
+				if (passwordEncoder.matches(password, usr.getPasswordHash()))
+				  { view= "redirect:/administrator/facultymanagement"; }
+			}
+			/*// reserve for student 
+			 * if (input.equals(usr.getUsername()) ) { if (passwordEncoder.matches(password,
+			 * usr.getPasswordHash())) { if(usr.getUserType() == "Student") view=
+			 * "redirect:/student/applycourse"; } }
+			 */
+			
+			/*// reserve for faculty 
+			 * if (input.equals(usr.getUsername()) ) { if (passwordEncoder.matches(password,
+			 * usr.getPasswordHash())) { if(usr.getUserType() == "Faculty") view=
+			 * "redirect:/faculty/faculty_home"; } }
+			 */
 			else
 			{
 				view = "login";
@@ -75,6 +95,10 @@ public class HomeController {
 		return view;
 	}
 	
-
+	
+	
 	
 }
+
+	
+
